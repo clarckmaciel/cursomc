cac72a0f4c326f2f0af5483bad250d07d1eb064a
Clarck Maciel <=>
2020-04-10 01:26:11 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/clarck/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.clarck.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.clarck.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+
+}
