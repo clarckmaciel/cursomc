63132a3b818441828e1ac300fda6b9c83d55967c
Clarck Maciel <=>
2020-04-14 23:02:47 -0300

    Pedido, Pagamento e EstadoPagamento

--- src/main/java/com/clarck/cursomc/CursomcApplication.java
+++ src/main/java/com/clarck/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.clarck.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.clarck.cursomc.domain.Cidade;
 import com.clarck.cursomc.domain.Cliente;
 import com.clarck.cursomc.domain.Endereco;
 import com.clarck.cursomc.domain.Estado;
+import com.clarck.cursomc.domain.Pagamento;
+import com.clarck.cursomc.domain.PagamentoComBoleto;
+import com.clarck.cursomc.domain.PagamentoComCartao;
+import com.clarck.cursomc.domain.Pedido;
 import com.clarck.cursomc.domain.Produto;
+import com.clarck.cursomc.domain.enums.EstadoPagamento;
 import com.clarck.cursomc.domain.enums.TipoCliente;
 import com.clarck.cursomc.repositories.CategoriaRepository;
 import com.clarck.cursomc.repositories.CidadeRepository;
 import com.clarck.cursomc.repositories.ClienteRepository;
 import com.clarck.cursomc.repositories.EnderecoRepository;
 import com.clarck.cursomc.repositories.EstadoRepository;
+import com.clarck.cursomc.repositories.PagamentoRepository;
+import com.clarck.cursomc.repositories.PedidoRepository;
 import com.clarck.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 
 	public static void main(String[] args) {
@@ -85,6 +97,25 @@ public class CursomcApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		//salvar primeiro o pedido, pois o pagamento depende do pedido
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
 		
 	}
 
--- src/main/java/com/clarck/cursomc/domain/Cliente.java
+++ src/main/java/com/clarck/cursomc/domain/Cliente.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -30,12 +30,15 @@ public class Cliente implements Serializable {
 	private Integer tipo;
 
 	@JsonManagedReference
-	@OneToMany(mappedBy = "cliente")	
-	private List<Endereco> enderecos  = new ArrayList<>();
-	
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+
 	@ElementCollection
-	@CollectionTable(name="TELEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>(); // Não aceita repetição
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 	}
@@ -103,6 +106,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
